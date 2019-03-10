3da728106a4097853e2a112bcec90deba9369bd3
MandSolo <mandcashin@hotmail.co.uk>
2019-03-10 01:33:07 +0000

    add tab.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Tab from "../src/components/Tab";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-       <h1>my tab app! </h1>
-        </header>
+        <Tab />
       </div>
     );
   }
---
+++ src/components/Tab.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Tab extends Component {
+    render() {
+      return (
+        <div className="Tab">
+        <h1>tabs will go here!</h1>
+        </div>
+      );
+    }
+  }
+  
+  export default Tab;
+  
