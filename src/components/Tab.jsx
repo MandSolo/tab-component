8508c7da7ce432c284cac696d2bc8995a7c085b8
MandSolo <mandcashin@hotmail.co.uk>
2019-03-10 02:07:52 +0000

    add data to state

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
 
 class Tab extends Component {
+  state = {
+    data: [
+      {
+        Title: "Tab One",
+        Text: `Follow the lay of the land. It's most important. If you do too much
+        it's going to lose its effectiveness. I'm a water fanatic. I love
+        water. I can't think of anything more rewarding than being able to
+        express yourself to others through painting.`
+      },
+      {
+        Title: "Tab Two",
+        Text: `This is a happy place, little squirrels live here and play. But
+        we're not there yet, so we don't need to worry about it. Painting
+        should do one thing. It should put happiness in your heart. That is
+        when you can experience true joy, when you have no fear.`
+      },
+      {
+        Title: "Tab Three",
+        Text: `Everything's not great in life, but we can still find beauty in it.
+        We'll put a happy little sky in here. In painting, you have
+        unlimited power. You have the ability to move mountains. Let the
+        paint work.`
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="Tab-container">
@@ -16,34 +42,9 @@ class Tab extends Component {
           </button>
         </div>
 
-        <div id="TabOne" className="Tab-content">
-          <h3>TabOne</h3>
-          <p>
-            Follow the lay of the land. It's most important. If you do too much
-            it's going to lose its effectiveness. I'm a water fanatic. I love
-            water. I can't think of anything more rewarding than being able to
-            express yourself to others through painting.
-          </p>
-        </div>
-
-        <div id="TabTwo" className="Tab-content">
-          <h3>TabTwo</h3>
-          <p>
-            This is a happy place, little squirrels live here and play. But
-            we're not there yet, so we don't need to worry about it. Painting
-            should do one thing. It should put happiness in your heart. That is
-            when you can experience true joy, when you have no fear.
-          </p>
-        </div>
-
-        <div id="TabThree" className="Tab-content">
-          <h3>TabThree</h3>
-          <p>
-            Everything's not great in life, but we can still find beauty in it.
-            We'll put a happy little sky in here. In painting, you have
-            unlimited power. You have the ability to move mountains. Let the
-            paint work.
-          </p>
+        <div className="Tab-content">
+          <h3>{this.state.data[0].Title}</h3>
+          <p>{this.state.data[0].Text}</p>
         </div>
       </div>
     );
